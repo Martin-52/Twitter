44f16ac5ff24634a30c261ca715df30f283aa03f
Martin Landin <martinlandin@martins-macbook-air.local>
2019-02-18 14:42:28 -0800

    Post tweet now available

---
+++ Twitter/TweetViewController.swift
@@ -0,0 +1,50 @@
+//
+//  TweetViewController.swift
+//  Twitter
+//
+//  Created by Martin Landin on 2/18/19.
+//  Copyright © 2019 Dan. All rights reserved.
+//
+
+import UIKit
+
+class TweetViewController: UIViewController {
+
+    @IBOutlet weak var tweetText: UITextView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tweetText.becomeFirstResponder()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    @IBAction func onCancel(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    @IBAction func onTweet(_ sender: Any) {
+        if(!tweetText.text.isEmpty) {
+            TwitterAPICaller.client?.postTweet(tweetString: tweetText.text, success: {
+                self.dismiss(animated: true, completion: nil)
+            }, failure: { (error) in
+                print("Error posting tweet \(error)")
+                self.dismiss(animated: true, completion: nil)
+            })
+        } else { /// TODO: Implement Alert Controller to notify no text inputed
+            print("Text needs to be inputed into tweet box")
+            self.dismiss(animated: true, completion: nil)
+        }
+    }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
